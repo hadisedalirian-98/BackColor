9c819ccd109430976527f4f0288eb14f2a82cfb4
hadisedalirian-98 <hadisdalirian5@gmail.com>
2025-02-21 13:08:24 +0330

    metro

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
+let citiDate = {  
+  tehran: { city: "tehran", temp: 12, weather: "sunny", humidity: 23, windSpeed: 32, winDirection: "to north", log: "Images/sunny.png", wallPaper: "Images/tehrancity.jpg" },  
+  shiraz: { city: "shiraz", temp: 8, weather: "storm", humidity: 19, windSpeed: 50, winDirection: "to north", log: "Images/storm.png", wallPaper: "Images/shiraz.jpg" },  
+  esfahan: { city: "esfahan", temp: 10, weather: "rainy", humidity: 24, windSpeed: 43, winDirection: "to north", log: "Images/rainy.png", wallPaper: "Images/esfahan.jpeg" }  
+};  
 
-let btn = document.getElementById("btn")
-let range = document.getElementById("range")
-let ms = document.getElementById("ms")
+let searchBtn = document.getElementById("search");  
+searchBtn.style.textAlign = "center";  
+let searchBar = document.getElementById("searchBar");  
 
-range.addEventListener("change",function(){
-  console.log(range.value)
-  ms.innerHTML=range.value
-})
-
-btn.addEventListener("click",function(){
-
-
-let redValue,greenValue,blueValue
-
-  setInterval(() => {
-    let redValue =Math.floor(Math.random()*255)
-    let greenValue =Math.floor(Math.random()*255)
-    let blueValue =Math.floor(Math.random()*255)
-   document.body.style.backgroundColor ="rgb("+ redValue+ ", "+ greenValue + ", " + blueValue +")"
-    
-  },range.value);
-
+searchBtn.addEventListener("click", function() {  
+  let searchBarValue = searchBar.value.toLowerCase(); // تغییر به حروف کوچک  
+  let cityData = citiDate[searchBarValue];   
   
-})
+  if (cityData) {  
+    document.querySelector(".city").innerHTML = "city: " + cityData.city;  
+    document.querySelector(".city").style.color = "white";  
+    document.querySelector(".city").style.textAlign = "center";  
+    
+    document.querySelector(".description").innerHTML = cityData.weather;  
+    document.querySelector(".description").style.color = "white";  
+    document.querySelector(".description").style.textAlign = "center";  
+    
+    document.querySelector(".temp").innerHTML = "temp is " + cityData.temp + "°C";  
+    document.querySelector(".temp").style.color = "white";  
+    document.querySelector(".temp").style.textAlign = "center";  
+    
+    document.querySelector(".humidity").innerHTML = "humidity: " + cityData.humidity;  
+    document.querySelector(".humidity").style.color = "white";  
+    document.querySelector(".humidity").style.textAlign = "center";  
+    
+    document.querySelector(".wind").innerHTML = "windSpeed is " + cityData.windSpeed + " K/h";  
+    document.querySelector(".wind").style.color = "white";  
+    document.querySelector(".wind").style.textAlign = "center";  
+    
+    document.querySelector(".windDirection").innerHTML = "winDirection: " + cityData.winDirection;  
+    document.querySelector(".windDirection").style.color = "white";  
+    document.querySelector(".windDirection").style.textAlign = "center";  
+    
+    document.querySelector(".icon").setAttribute("src", cityData.log);  
+    
+    document.getElementById("msg").style.display = "none";  
+    
+    document.body.style.backgroundImage = "url(" + cityData.wallPaper + ")"; // تغییر استفاده از backgroundImage  
+   
+  } else {  
+    document.getElementById("enterCity").innerHTML = "plz enter your city name correctly";   
+    document.getElementById("enterCity").style.color = "pink";  
+    document.getElementById("enterCity").style.textAlign = "center";  
+    document.getElementById("enterCity").style.fontSize = "25px";  
+    document.getElementById("enterCity").style.fontWeight = "bold";  
+    document.getElementById("msg").style.display = "none";  
+  }  
+});
